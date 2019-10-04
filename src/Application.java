c2b213249f2266d19feb5c7bd3f103d464de36c6
Edgar Perez <edgarperezblanco@gmail.com>
2019-10-04 19:09:44 +0200

    salu2

--- src/Application.java
+++ src/Application.java
@@ -1,12 +1,28 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Application {
     public static void main(String [] args)
     {
         System.out.println("Hola");
         System.out.println("ktal");
 
+        try (
+            FileReader reader = new FileReader("filename.txt");
+            BufferedReader br = new BufferedReader(reader)
+        ) {
+
+            // read line by line
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+        } catch (IOException e) {
+            System.err.format("IOException: %s%n", e);
+        }
     }
-
-
 
 
 }

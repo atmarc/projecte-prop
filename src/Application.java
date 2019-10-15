0e96bbcb87d95ec85901a1b24ae36af7558495a8
Edgar Perez <edgarperezblanco@gmail.com>
2019-10-15 16:07:53 +0200

    reformat

--- src/Application.java
+++ src/Application.java
@@ -4,38 +4,45 @@ import LZ78.LZ78_Compressor;
 import LZSS.LZSS_Compressor;
 import LZW.*;
 
+import java.io.File;
 import java.util.ArrayList;
 
 public class Application {
-    public static void main(String [] args) throws Exception
-    {
+    public static void main(String [] args) throws Exception {
         System.out.println("Testing output:\n");
 
-        ArrayList<String> paths = FileManager.readFolder("testing_files", ".ppm");
-        JpegAlgorithm.compress(FileManager.readFile(paths.get(0)));
-
-        /*
-        ArrayList<String> paths = FileManager.readFolder("testing_files", ".txt");
-        LZ78_Compressor LZ78 = new LZ78_Compressor();
-        LZ78.compress(FileManager.readFile(paths.get(0)));
-        LZ78.compress(FileManager.readFile_Byte(paths.get(0)));
-        */
 
 
-        /*
-        String data = FileManager.readFile("testing_files/filename.txt");
-        ArrayList<Integer> compressedData = LZWCompressor.compress(data);
-        FileManager.createFile(compressedData, "testing_files/output.zero");
-        compressedData = FileManager.readFileBytes("testing_files/output.zero");
-        String decompressedData = LZWDecompressor.decompress(compressedData);
-        FileManager.createFile(decompressedData, "testing_files/decompress.txt");
-        */
 
 
-        String path = FileManager.readFile("testing_files/filename.txt");
-        LZSS_Compressor LZSS = new LZSS_Compressor();
-        LZSS.Compress(path);
+        if (false) { // JPEG
+            ArrayList<String> paths = FileManager.readFolder("testing_files", ".ppm");
+            JpegAlgorithm.compress(FileManager.readFile(paths.get(0)));
+        }
 
+        if (true) { // LZ78
+            //ArrayList<String> paths = FileManager.readFolder("testing_files", ".txt");
+            LZ78_Compressor LZ78 = new LZ78_Compressor();
+            //LZ78.compress(FileManager.readFile(paths.get(0)));
+            LZ78.compress(FileManager.readFile("./testing_files/petit.txt"));
+            //LZ78.compress(FileManager.readFile_Byte(paths.get(0)));
+        }
+
+
+        if (false) { // LZW
+            String data = FileManager.readFile("testing_files/filename.txt");
+            ArrayList<Integer> compressedData = LZWCompressor.compress(data);
+            FileManager.createFile(compressedData, "testing_files/output.zero");
+            //compressedData = FileManager.readFileBytes("testing_files/output.zero");
+            String decompressedData = LZWDecompressor.decompress(compressedData);
+            FileManager.createFile(decompressedData, "testing_files/decompress.txt");
+        }
+
+        if (false) { // LZSS
+            String path = FileManager.readFile("testing_files/filename.txt");
+            LZSS_Compressor LZSS = new LZSS_Compressor();
+            LZSS.Compress(path);
+        }
 
     }
 

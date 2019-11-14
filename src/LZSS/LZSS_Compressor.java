ce16c68a69bb657d82a89d1b9c3c7ff507abcda3
Nico <nicase99@gmail.com>
2019-11-14 19:43:02 +0100

    commit nico

--- src/LZSS/LZSS_Compressor.java
+++ src/LZSS/LZSS_Compressor.java
@@ -1,7 +1,8 @@
 package LZSS;
 
-import LZSS.Pair;
-import Triplet.Triplet;
+import FileManager.FileManager;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.lang.String;
 import java.util.BitSet;
@@ -11,186 +12,325 @@ import java.util.Queue;
 
 public class LZSS_Compressor {
 
-    private Triplet<Integer, Integer, Character> dades;
-    //private Pair<Integer, Integer>() Intent;
-    private Queue<Pair<Integer,Integer>> coincidence = new LinkedList<>();
+
     private Queue<Character> charQ  = new LinkedList<>();
-    
 
 
-    private ArrayList<Triplet<Integer, Integer, Character>> myarray = new ArrayList<>();
-    private ArrayList<Character> searchBuffer = new ArrayList<>();
+    private boolean Coincidence(int i, int j, int window, String item, ArrayList<Character> searchBuffer) {
+        return i <= (window - 3) && searchBuffer.size() >= 3 &&
+                searchBuffer.get(j) == item.charAt(i) && searchBuffer.get(j + 1) == item.charAt(i + 1) &&
+                searchBuffer.get(j + 2) == item.charAt(i + 2);
+    }
 
+    private BitSet SubBitSet(BitSet s, int i, int j) {
 
+        BitSet aux = new BitSet(8);
+        int h = 0;
+
+        for (; i < j; ++i, h++) {
+            aux.set(h, s.get(i));
+        }
+
+        return aux;
+    }
+
+    private String BitSetToString(ArrayList<Boolean> s) {
+        String aux = "";
+        System.out.println(s.size());
+        for (int i = 0; i < s.size(); ++i) {
+            if (!s.get(i)) {
+                aux += "0";
+            }
+            else aux += "1";
+        }
+        return aux;
+    }
+
+    private static String stringBinToStringChar(String s) {
+        String retorn = "";
+        for (int j = 0; j + 16 <= s.length(); j += 16) {
+            char b = 0;
+            String aux = s.substring(j, j + 16);
+            for (int i = 0; i < 16; ++i) {
+                if (aux.charAt(i) == '1') {
+                    b = (char) (b | (1 << 15-i));
+                }
+            }
+            retorn = retorn + b;
+        }
+        return retorn;
+    }
     public LZSS_Compressor() {
     }
-/*
-    public ArrayList<Triplet<Integer, Integer, Character>> getArray() {
-        return myarray;
-    }
+    public void compress(String superItem) {
 
-    public void Compress (String item) {
+        boolean end = false;
+        int beggining = 0;
+        String item;
+        String result = "";
 
-        int size = item.length();
-        BitSet bits = new BitSet(size); // 1 si coincidencia, 0 si no
+        while (!end) {
+            ArrayList<Character> searchBuffer = new ArrayList<>(0);
 
-        char c;
-
-        for (int i = 0; i < size; ++i) {
-
-            c = item.charAt(i);
-            int despl = 0;
-            int offset = 0;
-            searchBuffer.add(c);
-
-            for (int j = 0; j < searchBuffer.size() - 1; ++j) { //-1 perque no es trobi a ell mateix (s'ha inserit en l'última posició)
-
-                if (searchBuffer.get(j) == c) {
-                    //System.out.println("searchjaja");
-                    offset = j;
-                    despl++;
-                    j++;
-                    int recent = 1;
-
-                    for (; j < searchBuffer.size() - recent && (i+1) < item.length(); j++, i++) { //recent augmenta cada cop que s'insereix un nou caràcter a searcBuffer per evitar que es trobin coincidencies amb ells mateixos
-                        bits.set(i, false);
-                        if (item.charAt(i + 1) == searchBuffer.get(j)) {
-                            bits.set(i, true);
-                            //System.out.println("dos");
-                            despl++;
-                            searchBuffer.add(item.charAt(i + 1));
-                            recent++;
-                        } else break;
-                    }
-                    break;
-                }
-
-            }
-
-            if (despl != 0) {
-                dades = new Triplet(offset, despl, '0'); //'0' = null
-                myarray.add(dades);
-                coincidence.add(new Pair<Integer, Integer>(offset, despl) );
-
-            } else {
-                dades = new Triplet(0, 0, c);
-                myarray.add(dades);
-                charQ.add(c);
-            }
-        }
-
-
-        for (int h = 0; h < myarray.size(); h++) {
-            System.out.println(myarray.get(h).getFirst().toString() + ", "  + myarray.get(h).getSecond().toString() + ", " + myarray.get(h).getThird().toString());
-            if (bits.get(h) == false ) {
-                char removed = charQ.remove();
-                System.out.println(removed);
+            if (superItem.length() > (beggining + 4095) ) {
+                item = superItem.substring(beggining, beggining + 4095);
+                beggining = beggining + 4095;
             }
             else {
-                Pair <Integer, Integer>  aux = coincidence.remove();
-                System.out.println(aux.getFirst() + ", " + aux.getSecond() );
-                h += (aux.getSecond() - 1);
+                item = superItem.substring(beggining ,superItem.length());
+                end = true;
             }
+
+            int window = item.length(); //preguntar com posar un valor de int pes petit de 16bits en un char
+
+            //BitSet bits = new BitSet(item.length());
+            ArrayList<Boolean> bits = new ArrayList<Boolean>();
+
+            short offset = 0;
+            short despl = -3;
+            boolean coinc;
+            int sizeQ;
+
+            for (int i = 0; i < (window); ++i) {
+
+                int nous = 0;
+                coinc = false;
+                despl = 0;
+                char c = item.charAt(i);
+                int size = searchBuffer.size();
+                int principi = 0;
+
+                for (short j = 0; j <= searchBuffer.size() - 3; ++j) {
+
+                    if (Coincidence(i,j,window,item,searchBuffer)) { //hem trobat una coincidencia de almenys llargada 3
+                        principi = i;
+                        despl = -2;
+                        nous++;
+                        searchBuffer.add(item.charAt(i));
+                        coinc = true;
+                        bits.add(true);
+                        offset = j;
+                        j++;
+                        i++;
+                        for (; j < searchBuffer.size() - nous && (i) < item.length(); j++,i++) {
+
+                            if (item.charAt(i) == searchBuffer.get(j)) {
+                                despl++;
+                                nous++;
+                                searchBuffer.add(item.charAt(i));
+                                bits.add(false);
+
+                            }
+                            else {
+                                i--;
+                                break;
+                            }
+                            if (despl == 15) break;
+                        }
+
+                        break;
+                    }
+                }
+
+                //System.out.println(i);
+                if (coinc) { //hem trobat coincidencia
+                    //posar offset en els 12 primers bits i despl en els 4 ultims dun char (16bits)
+                    //bits.set( (i-(despl+2)), true);
+                    //if (principi == 93) System.out.println("hello");
+                    short of = (short) (offset<<4);
+
+                    short desp = (short) (0x000F & despl); // es el desplaçament real -2 (coincidencies a partir de 3)
+
+                    char aux = (char) (of | desp); //12 bits de offset i 4 de desplaçament
+                    charQ.add(aux);
+                    sizeQ = charQ.size();
+                }
+                else {
+                    searchBuffer.add(item.charAt(i));
+                    bits.add(false);
+                    charQ.add(item.charAt(i));
+                    sizeQ = charQ.size();
+                }
+            }
+
+
+
+            String hola = BitSetToString(bits);
+            //System.out.println(hola);
+
+            int modhola = hola.length() % 16;
+
+            if (modhola != 0) {
+                String holaux = "";
+                for (int i = 0; i < (16 - modhola); ++i) {
+                    if (i != (16 - modhola -1) ) {
+                        holaux += "0";
+                    }
+                    else {
+                        holaux += "1";
+                    }
+                }
+                hola = holaux + hola;
+            }
+            else {
+                hola = "0000000000000001" + hola;
+            }
+
+            String jaja = stringBinToStringChar(hola);
+            //System.out.println(jaja);
+            System.out.println(jaja);
+
+            /*String result = "";
+            for (; charQ.size() != 0;) {
+                result += charQ.remove();
+            }
+            result += "-1";
+            for (int i = 0; i < item.length(); ++i) {
+                if (!bits.get(i)) {
+                    result += "0";
+                }
+                else result += "1";
+            }*/
+
+
+            result = result + jaja;
+            result = result + (char) 0xFFFF;
+            for (; charQ.size() != 0;) {
+                result += charQ.remove();
+            }
+            result = result + (char) 0xFFFF;
+            //System.out.println(result);
 
         }
 
+        try {
+            FileManager.createFile(result, "testing_files/Compressed.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-*/
-    public void compress(String item) {
 
-
-
+    /*public void compress2(String item) {
         int window = item.length(); //preguntar com posar un valor de int pes petit de 16bits en un char
 
-        BitSet bits = new BitSet(4096);
-
+        BitSet bits = new BitSet(item.length());
 
         short offset = 0;
         short despl = -3;
         boolean coinc;
+        int sizeQ;
 
-        for (int i = 0; i < window; ++i) {
+        for (int i = 0; i < (window); ++i) {
 
             int nous = 0;
             coinc = false;
             despl = 0;
             char c = item.charAt(i);
             int size = searchBuffer.size();
+            int principi = 0;
 
-            for (short j = 0; j < searchBuffer.size(); ++j) {
+            for (short j = 0; j <= searchBuffer.size() - 3; ++j) {
 
-                if (searchBuffer.size() >= 3 && searchBuffer.get(j) == item.charAt(i) && searchBuffer.get(j+1) == item.charAt(i+1) && searchBuffer.get(j+2) == item.charAt(i+2)) { //hem trobat una coincidencia de almenys llargada 3
+                if (Coincidence(i,j,window,item)) { //hem trobat una coincidencia de almenys llargada 3
+                    principi = i;
                     despl = -2;
                     nous++;
                     searchBuffer.add(item.charAt(i));
                     coinc = true;
                     offset = j;
                     j++;
-                    for (; j < searchBuffer.size() - nous && (i+1) < item.length(); j++, i++) {
+                    i++;
+                    for (; j < searchBuffer.size() - nous && (i) < item.length(); j++,i++) {
 
-                        if (item.charAt(i+1) == searchBuffer.get(j)) {
+                        if (item.charAt(i) == searchBuffer.get(j)) {
                             despl++;
                             nous++;
-                            searchBuffer.add(item.charAt(i+1));
+                            searchBuffer.add(item.charAt(i));
+
                         }
-                        else break;
+                        else {
+                            i--;
+                            break;
+                        }
+                        if (despl == 15) break;
                     }
+
                     break;
                 }
             }
 
-            if (coinc == true) { //hem trobat coincidencia
+            //System.out.println(i);
+            if (coinc) { //hem trobat coincidencia
                 //posar offset en els 12 primers bits i despl en els 4 ultims dun char (16bits)
-                bits.set( (i-(despl+2)), true);
+                //bits.set( (i-(despl+2)), true);
+                //if (principi == 93) System.out.println("hello");
+                bits.set(principi, true);
+                short of = (short) (offset<<4);
 
-                //System.out.println(despl);
-                short of = (short) (offset*16);
-                System.out.println(of);
-                short desp = (short) (15 & despl); // es el desplaçament real -2 (coincidencies a partir de 3)
-                System.out.println(desp);
+                short desp = (short) (0x000F & despl); // es el desplaçament real -2 (coincidencies a partir de 3)
 
-                char aux = (char) (of | desp);
-                System.out.print((short)aux);
+                char aux = (char) (of | desp); //12 bits de offset i 4 de desplaçament
                 charQ.add(aux);
+                sizeQ = charQ.size();
             }
             else {
                 searchBuffer.add(item.charAt(i));
                 bits.set(i, false);
                 charQ.add(item.charAt(i));
-                System.out.println(item.charAt(i));
+                sizeQ = charQ.size();
             }
         }
-/*
-        String result = "";
 
-        for (int i = 0; i < item.length(); ++i) {
-            if (bits.get(i) == false) {
-                char caux = charQ.remove();
-                //System.out.print(caux);
-                result += caux;
-            }
-            else {
 
-                char aux = charQ.remove();
-                short os = (short) (65520&aux);
-                short dpl = (short) (15&aux);
 
-                //i = i + (int) dpl;
+        String hola = BitSetToString(bits);
+        //System.out.println(hola);
 
-                //for (int j = (i - (int) os); j <= (j + (int) dpl); ++j) {
-                for (int j = 0; j < 2; ++j) {
-                    //System.out.print(result.charAt(j));
-                    result = result + result.charAt(j);
+        int modhola = hola.length() % 16;
+
+        if (modhola != 0) {
+            for (int i = 0; i < (16 - modhola); ++i) {
+                if (i != (16 - modhola -1) ) {
+                    hola = "0" + hola;
                 }
-
+                else {
+                    hola = "1" + hola;
+                }
             }
         }
-
-        for (int i = 0; i < result.length(); ++i) {
-            System.out.print(result.charAt(i));
-
+        else {
+            hola = "0000000000000001" + hola;
         }
 
-*/
-    }
+        String jaja = stringBinToStringChar(hola);
+        //System.out.println(jaja);
+        System.out.println(jaja);
+
+        String result = "";
+        for (; charQ.size() != 0;) {
+            result += charQ.remove();
+        }
+        result += "-1";
+        for (int i = 0; i < item.length(); ++i) {
+            if (!bits.get(i)) {
+                result += "0";
+            }
+            else result += "1";
+        }
+
+        /*String result = "";
+        result = result + jaja;
+        result = result + (char) 0xFFFF;
+        for (; charQ.size() != 0;) {
+            result += charQ.remove();
+        }
+        result = result + (char) 0xFFFF;
+
+
+        try {
+            FileManager.createFile(result, "testing_files/Compressed.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }*/
 }
--- src/LZSS/LZSS_Decompressor.java
+++ src/LZSS/LZSS_Decompressor.java
@@ -1,63 +1,108 @@
 package LZSS;
 
-import Triplet.Triplet;
+import FileManager.FileManager;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.lang.String;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class LZSS_Decompressor {
 
-    private Triplet<Integer, Integer, Character> dades;
-    private ArrayList<Triplet<Integer, Integer, Character>> myarray = new ArrayList<>();
+
     private ArrayList<Character> searchBuffer = new ArrayList<>();
-    private String result = "";
 
     public LZSS_Decompressor() {
     }
 
-    /*public void Decompress(ArrayList<Triplet<Integer, Integer, Character>> x) {
-        String result = "";
-        int size = x.size();
+    public void Decompress(String item) throws IOException {
+        int nextsegment = 0;
+        String resultat = "";
+        boolean end = false;
 
-        for (int i = 0; i < size; ++i) {
-            if (x.get(i).getFirst() == 0) {
-                result += x.get(i).getThird();
-            }
-            else {
-                int offset = x.get(i).getFirst();
-                int despl = x.get(i).getSecond();
-                int pos = result.length() - offset;
+        while (!end) {
 
-                for (int h = 0; h < despl; ++h) {
-                    result += result.charAt(pos);
-                    pos++;
+            Queue<Character> charQ  = new LinkedList<>();
+            ArrayList<Boolean> BitSet = new ArrayList<Boolean>();
+
+            boolean endBits = false;
+            int poscharQ = 0;
+            boolean start = false; //Els primers bits son per quadrar-los en multuiples de 8
+
+            for (int i = nextsegment; endBits == false; i++) {
+                char c = item.charAt(i);
+                if (start) {
+                    if (c == 0xFFFF) {
+                        poscharQ = i + 1;
+                        endBits = true;
+                    } else {
+
+                        for (int j = 0; j < 16; ++j) {
+                            char aux = (char) ((c >> 15 - j) & 0x0001);
+
+                            if (aux == 0) BitSet.add(false);
+                            else BitSet.add(true);
+                        }
+                    }
+                } else {
+                    if (c == 0x0001) {
+                        start = true;
+                    } else {
+                        boolean prefix = true;
+                        for (int h = 0; h < 16; ++h) {
+                            char aux = (char) ((c >> 15 - h) & 0x0001);
+                            if (prefix) {
+                                if (aux == 1) prefix = false;
+                            } else {
+                                if (aux == 0) BitSet.add(false);
+                                else BitSet.add(true);
+                            }
+                        }
+                        start = true;
+                    }
                 }
             }
-        }
 
-        for (int j = 0; j < result.length(); ++j) {
-            System.out.print(result.charAt(j));
-        }
-    }*/
-/*
-    public void Decompress (item) {
-        //Read bitset, coincidence and charQ
-
-        for (int i; i < bits.size(); ++i) {
-
-            if (bits.get(i) == false) {
-                result += charQ.remove();
+            for (int i = poscharQ; i < item.length(); i++) {
+                if (item.charAt(i) == 0xFFFF) { //Vol dir que s'ha acabat aquest segment
+                    nextsegment = i + 1;
+                    if (nextsegment >= item.length()) end = true;
+                    break;
+                } else charQ.add(item.charAt(i));
             }
-            else {
-                Pair <Integer, Integer> aux = coincidence.remove();
-                int h = i;
-                for (int j = 0; j < aux.getSecond(); ++j) {
 
-                    result = result + result.charAt(h - aux.getFirst());
-                    h++;
+
+            for (int i = 0; i < BitSet.size(); ++i) {
+                if (!BitSet.get(i)) {
+                    resultat += (char) charQ.remove();
+
+                } else {
+                    char aux = charQ.remove();
+                    short os = (short) (aux & 0xFFF0);
+                    os = (short) (os >> 4);
+                    os = (short) (os & 0x0FFF);
+                    short dpls = (short) (aux & 0x000F);
+                    dpls = (short) (2 + dpls);
+                    short point = (short) (os);
+
+                    for (short j = 0; j <= dpls; ++j) {
+
+                        resultat = resultat + resultat.charAt(point + j);
+                    }
+                    i += dpls;
+
                 }
-                i += aux.getSecond();
             }
+
         }
+
+        try {
+            FileManager.createFile(resultat, "testing_files/result.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
     }
-*/}
+}

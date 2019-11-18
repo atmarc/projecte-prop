a3bbeb5a0680e059488486a395e08879b23c3676
andrei.mihalache <andrei.mihalache@est.fib.upc.edu>
2019-11-18 12:16:57 +0100

    Test OK

--- tests/Compressor_LZWTest.java
+++ tests/Compressor_LZWTest.java
@@ -23,36 +23,44 @@ public class Compressor_LZWTest {
 	@Test
 	public void compress1() throws IOException {
 		Compressor_Controller cc = new Compressor_Controller(2);
-		cc.startCompression("persistencia/testing_files/txt/good_one.txt");
+		cc.startCompression("src/persistencia/testing_files/txt/good_one.txt",
+				"src/persistencia/testing_files/txt/");
 		Decompressor_Controller dc = new Decompressor_Controller("lzw");
-		dc.startDecompression("persistencia/testing_files/txt/good_one.lzw");
-		assertTrue(diffFiles("persistencia/testing_files/txt/good_one_decompressed.txt",
-				"persistencia/testing_files/txt/good_one.txt"));
+		dc.startDecompression("src/persistencia/testing_files/txt/good_one.lzw",
+				"src/persistencia/testing_files/txt/");
+		assertTrue(diffFiles("src/persistencia/testing_files/txt/good_one_decompressed.txt",
+				"src/persistencia/testing_files/txt/good_one.txt"));
 	}
 
 	@Test
 	public void compress2() throws IOException {
 		Compressor_Controller cc = new Compressor_Controller(2);
-		cc.startCompression("persistencia/testing_files/txt/big.txt");
+		cc.startCompression("src/persistencia/testing_files/txt/big.txt",
+				"src/persistencia/testing_files/txt/");
 		Decompressor_Controller dc = new Decompressor_Controller("lzw");
-		dc.startDecompression("persistencia/testing_files/txt/big.lzw");
-		assertTrue(diffFiles("persistencia/testing_files/txt/big_decompressed.txt",
-				"persistencia/testing_files/txt/big.txt"));
+		dc.startDecompression("src/persistencia/testing_files/txt/big.lzw",
+				"src/persistencia/testing_files/txt/");
+		assertTrue(diffFiles("src/persistencia/testing_files/txt/big_decompressed.txt",
+				"src/persistencia/testing_files/txt/big.txt"));
 	}
 
 	@Test
 	public void compress3() throws IOException {
 		Compressor_Controller cc = new Compressor_Controller(2);
-		cc.startCompression("persistencia/testing_files/txt/emoji.txt");
+		cc.startCompression("src/persistencia/testing_files/txt/emoji.txt",
+				"src/persistencia/testing_files/txt/");
 		Decompressor_Controller dc = new Decompressor_Controller("lzw");
-		dc.startDecompression("persistencia/testing_files/txt/emoji.lzw");
-		boolean r1 = diffFiles("persistencia/testing_files/txt/emoji_decompressed.txt",
-				"persistencia/testing_files/txt/emoji.txt");
+		dc.startDecompression("src/persistencia/testing_files/txt/emoji.lzw",
+				"src/persistencia/testing_files/txt/");
+		boolean r1 = diffFiles("src/persistencia/testing_files/txt/emoji_decompressed.txt",
+				"src/persistencia/testing_files/txt/emoji.txt");
 
-		cc.startCompression("persistencia/testing_files/txt/japones.txt");
-		dc.startDecompression("persistencia/testing_files/txt/japones.lzw");
-		boolean r2 = diffFiles("persistencia/testing_files/txt/japones_decompressed.txt",
-				"persistencia/testing_files/txt/japones.txt");
+		cc.startCompression("src/persistencia/testing_files/txt/japones.txt",
+				"src/persistencia/testing_files/txt/");
+		dc.startDecompression("src/persistencia/testing_files/txt/japones.lzw",
+				"src/persistencia/testing_files/txt/");
+		boolean r2 = diffFiles("src/persistencia/testing_files/txt/japones_decompressed.txt",
+				"src/persistencia/testing_files/txt/japones.txt");
 		assertTrue(r1 && r2);
 	}
 

103917060653b5338fcbd5e13d09b99da403c8cf
atmarc <marquitus99@gmail.com>
2019-11-18 10:20:40 +0100

    tests i files

---
+++ tests/Compressor_JPEGTest.java
@@ -0,0 +1,55 @@
+import org.junit.Test;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Compressor_JPEGTest {
+
+    private static boolean diffFiles(String name1, String name2) throws IOException {
+        File f1 = new File(name1);
+        File f2 = new File(name2);
+        if (f1.length() != f2.length()) return false;
+        BufferedInputStream in1 = new BufferedInputStream(new FileInputStream(f1.getPath()));
+        BufferedInputStream in2 = new BufferedInputStream(new FileInputStream(f2.getPath()));
+        int b1, b2;
+        while ((b1 = in1.read()) != -1) {
+            b2 = in2.read();
+            if (b1 != b2) return false;
+        }
+        in1.close();
+        in2.close();
+        return true;
+    }
+
+
+    @Test
+    public void mainTests() throws IOException {
+
+        Compressor_Controller compressor_jpeg = new Compressor_Controller(3);
+        Decompressor_Controller decompressor_jpeg = new Decompressor_Controller("jpeg");
+
+        String[] files = {
+                "./testing_files/ppm_images/boxes_1",
+                "./testing_files/ppm_images/image",
+                "./testing_files/ppm_images/house_1",
+                "./testing_files/ppm_images/west_1",
+                "./testing_files/ppm_images/france-wallpaper"
+        };
+
+        for (int i = 0; i < files.length; ++i) {
+            compressor_jpeg.startCompression(files[i] + ".ppm", null);
+            System.out.println();
+            System.out.println("Verdict: " + (diffFiles(files[i]+"_test.jpeg", files[i] + ".jpeg")
+                    ? "\u001B[32m" + "OK! Files are equal." + "\u001B[0m"
+                    : "\u001B[31m" + "Wrong!!!" + "\u001B[0m"));
+            System.out.println();
+            decompressor_jpeg.startDecompression(files[i] + ".jpeg", null);
+            System.out.println();
+            System.out.println("-------------------------------------------------");
+        }
+    }
+
+}

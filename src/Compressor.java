421ce153453c1a28e689ba285bf375448901bc60
Edgar <edgarperezblanco@gmail.com>
2019-11-18 03:38:57 +0100

    Documentacion Controladoras

--- src/Compressor.java
+++ src/Compressor.java
@@ -1,16 +1,26 @@
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
+/**
+ * Clase con metodos abstractos para implementar un compresor. Contiene una variable que hace referencia a su controlador para poder comunicarse con otras capas.
+ */
 public abstract class Compressor {
 
+    /**
+     * Setter de la controladora.
+     * @param controller Controladora del compresor.
+     */
     public void setController(Compressor_Controller controller) {
         this.controller = controller;
     }
 
-    protected Compressor_Controller controller;
+    protected Compressor_Controller controller; ///< Controlador del compresor.
 
+    /**
+     * Retorna la extension del archivo comprimido (depende del tipo de algoritmo utilizado).
+     * @return Retorna la extension del archivo comprimido.
+     */
     protected abstract String getExtension();
+    /**
+     * Funcion encargada de comprimir el archivo proporcionado por la controladora y escribirlo en el archivo de salida tambien a traves de la controladora.
+     */
     protected abstract void compress();
 
 }
--- src/Decompressor.java
+++ src/Decompressor.java
@@ -1,17 +1,26 @@
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
+/**
+ * Clase con metodos abstractos para implementar un descompresor. Contiene una variable que hace referencia a su controlador para poder comunicarse con otras capas.
+ */
 public abstract class Decompressor {
 
-
+    /**
+     * Setter de la controladora.
+     * @param controller Controladora del descompresor.
+     */
     public void setController(Decompressor_Controller controller) {
         this.controller = controller;
     }
 
-    protected Decompressor_Controller controller;
+    protected Decompressor_Controller controller; ///< Controlador del descompresor.
 
+    /**
+     * Retorna la extension del archivo original (actualemente comprimido).
+     * @return Retorna la extension del archivo original.
+     */
     protected abstract String getExtension();
+    /**
+     * Funcion encargada de descomprimir el archivo proporcionado por la controladora y escribirlo en el archivo de salida tambien a traves de la controladora.
+     */
     protected abstract void decompress();
 
 }

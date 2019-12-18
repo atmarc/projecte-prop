f88fce03e84ca4d0fc3b4173ebeb92e79aff3d3e
Nico <nicase99@gmail.com>
2019-12-18 23:03:00 +0100

    pair

---
+++ src/dominio/Pair.java
@@ -0,0 +1,14 @@
+package dominio;
+
+public class Pair<L,R> {
+    private Integer l;
+    private Byte r;
+    public Pair(Integer l, Byte r){
+        this.l = l;
+        this.r = r;
+    }
+    public Integer getL(){ return l; }
+    public Byte getR(){ return r; }
+    public void setL(Integer l){ this.l = l; }
+    public void setR(Byte r){ this.r = r; }
+}

3314dae8feebfb418efcf94573b8f7e33c6588e6
Nico <nicase99@gmail.com>
2019-12-19 01:00:24 +0100

    gui

--- src/presentacion/End.java
+++ src/presentacion/End.java
@@ -1,13 +1,14 @@
 package presentacion;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class End {
     private JPanel panel1;
     private JLabel foto;
     private JTextArea text;
-
-
+    private JButton inicioButton;
 
 
     public End(Presentation_Controller presentation_controller) {
@@ -20,6 +21,14 @@ public class End {
         ImageIcon icon = new ImageIcon(getClass().getResource("egg/resized3.gif"));
         foto.setIcon(icon);
 
+        inicioButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                presentation_controller.setVariables();
+                presentation_controller.switchToWelcome();
+
+            }
+        });
     }
 
     public JPanel getPanel1() {
